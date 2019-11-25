7683281e167a1b7235b62e4fd1dbbe14f44cb494
Jose Carballosa <jose.carballosa@gmail.com>
2019-11-25 12:54:33 +0100

    chore: refactored build script and remote repo in package.json

--- copy-assets.ts
+++ copy-assets.ts
@@ -1,3 +1,4 @@
 import shell from 'shelljs';
 
 shell.cp('-R', 'src/public', 'dist');
+shell.cp('-R', 'src/app/views', 'dist/app');
--- copy-views.ts
+++
@@ -1,3 +0,0 @@
-import shell from 'shelljs';
-
-shell.cp('-R', 'src/app/views', 'dist/app');
